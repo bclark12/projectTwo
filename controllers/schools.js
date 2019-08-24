8d1e60fe67833f71875403409df9ae996126015b
Barrett Clark <barrettclark@Barretts-MacBook-Pro.local>
2019-08-24 00:39:08 -0400

    found way to loop json object keys

--- controllers/schools.js
+++ controllers/schools.js
@@ -2,14 +2,30 @@ const express = require('express')
 const schoolsApi = require('../models/schools.js')
 const statesApi = require('../models/states.js')
 const schoolsRouter = express.Router()
-
-schoolsRouter.get('/:stateId', (req, res) => {
+//try loop here
+console.log(statesApi.getStates().then(states => {
+    console.log(states[0].state)
+    })
+)
+statesApi.getStates().then(states => {
+schoolsRouter.get('/'+ states[0].state +'/:stateId', (req, res) => {
     statesApi.getState(req.params.stateId).then(stateInDB => {
         schoolsApi.getSchools().then(schoolsInDB => {      
             res.render('schools', {schoolsInDB, stateInDB, _id: req.params.stateId})
         })
     })
 })
+})
+
+// console.log(statesApi.getStates().then(states => {
+//     console.log(states[0].state)
+//     })
+// )
+statesApi.getStates().then(states => {
+    return states[0].states
+})
+
+
 
 schoolsRouter.get('/addSchool/:stateId', (req, res) => {
     statesApi.getState(req.params.stateId).then(stateInDB => {
