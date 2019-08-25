b38e38c7e8889585f38333ac46a29b9cf44a76ab
Barrett Clark <barrettclark@Barretts-MacBook-Pro.local>
2019-08-25 09:57:53 -0400

    Finally defeated the problem ive had for 2 days

--- controllers/schools.js
+++ controllers/schools.js
@@ -11,21 +11,46 @@ console.log(statesApi.getStates().then(states => {
             statesApi.getState(req.params.stateId).then(stateInDB => {               
                 schoolsApi.getSchools().then(schoolsInDB => {
                     console.log(schoolsInDB[14].stateId)
-                    console.log(schoolsInDB.length)  //schoolsInDB is an array
+                    console.log(schoolsInDB)  //schoolsInDB is an array
                     //loop here  
+                    const matchingSchools = []
                     for (j = 0; j < schoolsInDB.length; j++) {
                         console.log(j)
                         if (schoolsInDB[j].stateId == req.params.stateId) {
-                            console.log(schoolsInDB[j].name)
-                        }
+                            console.log(schoolsInDB[j])
+                            matchingSchools.push(schoolsInDB[j])
+                        }                    
                     }
-                    statesApi.getStates().then(states => {
-                        console.log(states[0].state)
+                    let testSchools = [
+                      {
+                        _id: '5d61e56cad02c743de1e3844',
+                        name: 'nostatename',
+                        state: 'Idaho',
+                        students: 32523,
+                        mascot: 'asdfg',
+                        athleticsRank: 5,
+                        stateId: '5d61e002be6c234321ee6e4d',
+                        __v: 0
+                      },
+                      {
+                        _id: '5d628e62306f0d4519f51568',
+                        name: 'idaho college',
+                        state: '',
+                        students: 2344,
+                        mascot: 'asss',
+                        athleticsRank: 6,
+                        stateId: '5d61e002be6c234321ee6e4d',
+                        __v: 0
+                      }
+                    ]
+                    //statesApi.getStates().then(states => {
+                        //console.log(states[0].state)
                                                                
-                        res.render('schools', {schoolsInDB, stateInDB, _id: req.params.stateId})
+                        res.render('schools', {schoolsInDB, stateInDB, testSchools, matchingSchools, _id: req.params.stateId})
+                    
                     
                   
-                    })
+                    //})
                 })
             })
         })
@@ -43,7 +68,7 @@ schoolsRouter.get('/addSchool/:stateId', (req, res) => {
 statesApi.getStates().then(states => {
     for (i = 0; i < states.length; i++) {
         //schoolsRouter.post('/:stateId', (req, res) => {
-        schoolsRouter.post('/' + states[i].state + '/:stateId', (req, res) => {
+        schoolsRouter.post('/:stateId', (req, res) => {
             schoolsApi.addSchool(req.body).then(() => {
                 res.send('worked')
             })
